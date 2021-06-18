27ea190c3047392cc8f11ac8a58ec198c5c978c0
Ryan Sears <ryan@calidog.io>
2021-06-18 02:02:22 -0400

    Redo cluster analysis to fix some bugs
    Before we weren't removing common clusters between analysis and pulling from pre-computed data (and even forgot to replace people's points post-cluster), this will calculate everything in one place and should be correct.

---
+++ analysis/analysis.py
@@ -0,0 +1,110 @@
+import requests
+import csv
+from io import StringIO
+from xtz_analysis_clusters import xtz_analysis_clusters
+from funding_source_analysis_clusters import funding_source_analysis_clusters
+
+flatten = lambda t: [item for sublist in t for item in sublist]
+
+def fetch_category(url, name):
+    print("Fetching {}".format(name))
+    airdrop_data = requests.get(url)
+    cleaned_data = [x.strip().rstrip(',').replace(', ', ',') for x in airdrop_data.content.decode().split('\n') if x]
+
+    data_strio = StringIO('\n'.join(cleaned_data))
+
+    addresses = {}
+
+    for airdrop_recipient in csv.DictReader(data_strio):
+        addresses[airdrop_recipient['address']] = int(airdrop_recipient['points'])
+
+    return addresses
+
+datasets = {
+    'Dexter_LP': fetch_category('https://raw.githubusercontent.com/Hover-Labs/kdao-airdrop/main/dexter-lps.csv', "Dexter LP list"),
+    'Gov_Executor': fetch_category('https://raw.githubusercontent.com/Hover-Labs/kdao-airdrop/main/governance.csv', "Gov Executor list"),
+    'Kolibri_LP': fetch_category('https://raw.githubusercontent.com/Hover-Labs/kdao-airdrop/main/kolibri-lps.csv', "Kolibri LP list"),
+    'Protocol_User': fetch_category('https://raw.githubusercontent.com/Hover-Labs/kdao-airdrop/main/oven-borrowers.csv', "Protocol User list"),
+    'Quipu_LP_New': fetch_category('https://raw.githubusercontent.com/Hover-Labs/kdao-airdrop/main/quipu-lps-new.csv', "Quipuswap (new) list"),
+    'Quipu_LP_Old': fetch_category('https://raw.githubusercontent.com/Hover-Labs/kdao-airdrop/main/quipu-lps-old.csv', "Quipuswap (old) list"),
+}
+
+clusters_to_remove_from_funding_analysis = set()
+
+final_clusters = {}
+
+for xtz_analysis_cluster, xtz_analysis_addresses in xtz_analysis_clusters.items():
+    final_clusters["xtz_analysis_" + xtz_analysis_cluster] = xtz_analysis_addresses
+    for funding_analysis_cluster, funding_analysis_addresses in funding_source_analysis_clusters.items():
+        intersection = xtz_analysis_addresses.intersection(funding_analysis_addresses)
+        if len(intersection) > 0:
+            similarity = len(intersection) / len(xtz_analysis_addresses)
+            if similarity == 1:
+                clusters_to_remove_from_funding_analysis.add(funding_analysis_cluster)
+            # if similarity < 1:
+            #     print("Cluster (xtz analysis {}) and (funding analysis {}) are similar ({}%)".format(xtz_analysis_cluster, funding_analysis_cluster, similarity * 100))
+            #     print("  - LEFT: ", xtz_analysis_addresses.difference(funding_analysis_addresses))
+            #     print("  - RIGHT: ", funding_analysis_addresses.difference(xtz_analysis_addresses))
+
+for funding_analysis_cluster, funding_analysis_addresses in funding_source_analysis_clusters.items():
+    if funding_analysis_cluster not in clusters_to_remove_from_funding_analysis:
+        final_clusters["funding_analysis_" + funding_analysis_cluster] = funding_analysis_addresses
+
+blacklist = set()
+airdrop_replacements = {}
+for cluster_name, cluster_addreses in final_clusters.items():
+    sorted_addresses = sorted(cluster_addreses, key=str.casefold)
+    to_be_airdropped, to_be_blacklisted = sorted_addresses[0], set(sorted_addresses[1:])
+    blacklist = blacklist.union(to_be_blacklisted)
+
+    cluster_activities = set()
+
+    for activity, collection in datasets.items():
+        for address in cluster_addreses:
+            if address in collection:
+                cluster_activities.add(activity)
+
+    airdrop_replacements[to_be_airdropped] = len(cluster_activities)
+
+blacklist = blacklist.union({
+ 'tz1Mdtt1jtPiMmKVSDbB2PRHVVRcVStayanS',
+ 'tz1Mqnms73LqgBCYiM7e5k12VyWNQG8ytcGb',
+ 'tz1UKJ6X9pfAneU62ndcXukNL3rPzxCskT6K',
+ 'tz1VmeVuNCpwi3uuhBGWpYFk1Qx8UKYpZsJi',
+ 'tz1eDj5UuChVcZpA7gofUtyVS6mdQAcyEbZ5',
+ 'tz1hoverof3f2F8NAavUyTjbFBstZXTqnUMS',
+})
+
+point_totals = {}
+for activity, collection in datasets.items():
+    participating_addresses = set(collection.keys())
+
+    filtered_addresses = participating_addresses - blacklist
+
+    for address in filtered_addresses:
+        if address not in point_totals:
+            point_totals[address] = 1
+        else:
+            point_totals[address] += 1
+
+for address, replacement_value in airdrop_replacements.items():
+    point_totals[address] = replacement_value
+
+total_points = sum(point_totals.values())
+
+# 150,000 kDAO earmarked for the airdrop
+TOKEN_ALLOCATION = 150_000
+
+split_per_point = TOKEN_ALLOCATION / total_points
+
+print('New point allocation per address {}'.format(split_per_point))
+
+csv_header = ['address, token_allocation']
+csv_data = []
+for address, points in point_totals.items():
+    csv_data += ["{}, {}".format(address, points * split_per_point)]
+
+with open('airdrop-data.csv', 'w') as f:
+    f.write('\n'.join(csv_header + sorted(csv_data, key=str.casefold)))
+
+print("Wrote airdrop data to airdrop-data.csv")
